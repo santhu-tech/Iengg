b90889b7041a7a0a0ae992ddeb57ae7b67431835
santhu-tech <psanthiyamahe@gmail.com>
2024-11-27 12:18:04 +0530

    Add files via upload

---
+++ kafka-producer-example-main/kafka-producer-example-main/src/main/java/com/javaiEngg/controller/EventController.java
@@ -0,0 +1,45 @@
+package com.javaiEngg.controller;
+
+//import com.javaiEngg.dto.Customer;
+import com.javaiEngg.service.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher publisher;
+
+    @GetMapping("/publish/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String message) {
+        try {
+            publisher.sendMessageToTopic(message);
+            return ResponseEntity.ok("message published successfully ..");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+      /*  try {
+            //for (int i = 0; i <= 100000; i++) {
+               // publisher.sendMessageToTopic(message + " : " + i);
+            publisher.sendMessageToTopic(message );
+            }
+            return ResponseEntity.ok("message published successfully ..");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }*/
+   // }
+
+//    @PostMapping("/publish")
+//    public void sendEvents(@RequestBody Customer customer) {
+//        publisher.sendEventsToTopic(customer);
+//    }
+//
+
+}
---
+++ kafka-producer-example-main/kafka-producer-example-main/src/main/java/com/javaiEngg/service/KafkaMessagePublisher.java
@@ -0,0 +1,33 @@
+package com.javaiEngg.service;
+
+//import com.javaiEngg.dto.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    public void sendMessageToTopic(String message) {
+        //template.send("iEngg-demo")
+        CompletableFuture<SendResult<String, Object>> future = template.send("iEngg-demo1", message);
+       // future.get();
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        message + "] due to : " + ex.getMessage());
+            }
+        });
+
+    }
+
+}
