3d76e2de1d0a00fb459eb0eea10003c037fc36e0
santhu-tech <psanthiyamahe@gmail.com>
2024-11-29 12:04:02 +0530

    Day-3

---
+++ spring-kafka-avro-main/spring-kafka-avro-main/src/main/java/com/iEngg/controller/EventController.java
@@ -0,0 +1,20 @@
+package com.iEngg.controller;
+
+import com.iEngg.dto.Employee;
+import com.iEngg.producer.KafkaAvroProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    @Autowired
+    private KafkaAvroProducer producer;
+
+    @PostMapping("/events")
+    public String sendMessage(@RequestBody Employee employee) {
+        producer.send(employee);
+        return "message published !";
+    }
+}
---
+++ spring-kafka-avro-main/spring-kafka-avro-main/src/test/java/com/iEngg/KafkaSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iEngg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSchemaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
